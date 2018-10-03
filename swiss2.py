d979020b888147b77189f6a203bb00b05b404010
Eugene Nekrasov <nekrasov@mlsco.ru>
2018-10-03 23:23:38 +0500

    Улучшил пример

--- swiss2.py
+++ swiss2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Queue import LifoQueue
+from Queue import LifoQueue, Full
 
 # Потокобезопасная очередь размер в 5 элементов
 bucket = LifoQueue(5)
@@ -8,8 +8,8 @@ def putToBucket(el):
   # Метод принимает элемент и если очередь полна эелемент не попадает в очередь
   try:
     bucket.put_nowait(el)
-  except:
-    pass
+  except Full as e:
+    print("Очередь полна и не может принять элемент %s" % el)
 
 for i in range(0, 9):
   putToBucket(i)
